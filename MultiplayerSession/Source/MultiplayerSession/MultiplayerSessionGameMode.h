b542fa3d0d17abbb2ae3e7631036826b49d0b733
Alejandro Abecasis <ale.a.abecasis@gmail.com>
2022-07-04 19:05:49 -0300

    Added initial project

---
+++ MultiplayerSession/Source/MultiplayerSession/MultiplayerSessionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerSessionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiplayerSessionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerSessionGameMode();
+};
+
+
+
